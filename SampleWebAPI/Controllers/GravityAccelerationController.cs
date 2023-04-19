c0370c7baf35f1815ce1b299cec186dd525fb31d
Фирстов Дмитрий Игоревич <d.firstov@tinkoff.ru>
2023-04-19 14:11:26 +0300

    Produce message when creating a new acceleration

--- SampleWebAPI/Controllers/GravityAccelerationController.cs
+++ SampleWebAPI/Controllers/GravityAccelerationController.cs
@@ -1,5 +1,8 @@
+using System.Globalization;
+using Confluent.Kafka;
 using Microsoft.AspNetCore.Mvc;
 using SampleWebAPI.Data;
+using SampleWebAPI.Kafka;
 using SampleWebAPI.Models;
 
 namespace SampleWebAPI.Controllers;
@@ -9,10 +12,14 @@ namespace SampleWebAPI.Controllers;
 public class GravityAccelerationController : ControllerBase
 {
 	private readonly GravityAccelerationContext _gravityAccelerationContext;
+	private readonly KafkaProducer _kafkaProducer;
 
-	public GravityAccelerationController(GravityAccelerationContext gravityAccelerationContext)
+	public GravityAccelerationController(
+		GravityAccelerationContext gravityAccelerationContext,
+		KafkaProducer kafkaProducer)
 	{
 		_gravityAccelerationContext = gravityAccelerationContext;
+		_kafkaProducer = kafkaProducer;
 	}
 
 	[HttpPost]
@@ -20,6 +27,12 @@ public class GravityAccelerationController : ControllerBase
 	{
 		_gravityAccelerationContext.GravityAccelerations.Add(gravityAcceleration);
 		await _gravityAccelerationContext.SaveChangesAsync();
+
+		await _kafkaProducer.ProduceAsync(new Message<string, string>()
+		{
+			Key = gravityAcceleration.Name,
+			Value = gravityAcceleration.Value.ToString(CultureInfo.InvariantCulture)
+		});
 
 		return Ok();
 	}
