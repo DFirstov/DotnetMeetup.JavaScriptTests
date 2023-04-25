d842ee230fa3c9c3370eb04ef1d48dc6ecc93c88
Фирстов Дмитрий Игоревич <d.firstov@tinkoff.ru>
2023-04-25 11:40:28 +0300

    Refactor FallingTimeController tests

--- SampleWebAPI/Controllers/FallingTimeController.test.js
+++ SampleWebAPI/Controllers/FallingTimeController.test.js
@@ -1,115 +1,152 @@
-const axios = require('axios');
-const knex = require("knex");
-const uuid = require("uuid");
+const axios = require('axios')
+const knex = require("knex")
+const uuid = require("uuid")
 
-test('GET FallingTime without GA name returns default value', async () => {
-    const response = await axios.get('http://localhost:5204/FallingTime');
+test('GET FallingTime without GA returns default GA', async () => {
+    // Act
+    const url = 'http://localhost:5204/FallingTime'
+    const response = await axios.get(url)
+
+    // Assert
     expect(response.data['gravityAcceleration']).toEqual({
         name: 'default',
         value: 9.81
-    });
-});
+    })
+})
 
 describe.each([
     [0, 0.00],
     [1, 0.45],
     [5, 1.01]
-])('startHeight = %s', (startHeight, expectedFallingTime) => {
-    test(`GET FallingTime for startHeight = ${startHeight} returns fallingTime = ${expectedFallingTime}`, async () => {
-        const response = await axios.get(`http://localhost:5204/FallingTime?startHeight=${startHeight}`);
-        expect(response.data['value']).toBeCloseTo(expectedFallingTime, 2);
-    });
-});
+])('startHeight = %s', (startHeight, expectedTime) => {
+    test('GET FallingTime without GA returns correct falling time', async () => {
+        // Act
+        const url = `http://localhost:5204/FallingTime?startHeight=${startHeight}`
+        const response = await axios.get(url)
+
+        // Assert
+        expect(response.data['value']).toBeCloseTo(expectedTime, 2)
+    })
+})
 
 test('GET FallingTime for negative startHeight returns 400', async () => {
-    const response = await axios.get('http://localhost:5204/FallingTime?startHeight=-1', {validateStatus: () => true});
-    expect(response.status).toBe(400);
-});
+    // Act
+    const url = 'http://localhost:5204/FallingTime?startHeight=-1'
+    const response = await axios.get(url, {validateStatus: () => true})
+
+    // Assert
+    expect(response.status).toBe(400)
+})
 
 describe.each([
     [0, 1, 0.00],
     [1, 1, 1.41],
     [5, 6, 1.29]
-])('startHeight = %s, ga = %s', (startHeight, ga, expectedFallingTime) => {
-    test(`GET FallingTime for startHeight = ${startHeight} and GA = ${ga} in DB returns fallingTime = ${expectedFallingTime}`, async () => {
-        const dbClient = knex({
-            client: 'pg',
-            connection: {
-                host: 'localhost',
-                port: '5432',
-                user: 'postgres',
-                password: 'js-tests'
-            }
-        });
+])('startHeight = %s, ga = %s', (startHeight, ga, expectedTime) => {
+    test('GET FallingTime with GA returns correct falling time', async () => {
+        // Arrange
+        const gaName = uuid.v4()
+        await configureGravityAccelerationMockOK(gaName, ga)
 
-        const gaData = {
-            Name: uuid.v4(),
-            Value: ga
-        };
+        // Act
+        const url = `http://localhost:5204/FallingTime?startHeight=${startHeight}&gaName=${gaName}`
+        const response = await axios.get(url)
 
-        await dbClient
-            .insert(gaData)
-            .into('GravityAcceleration');
-
-        const response = await axios.get(`http://localhost:5204/FallingTime?startHeight=${startHeight}&gaName=${gaData.Name}`);
-
-        expect(response.data['value']).toBeCloseTo(expectedFallingTime, 2);
-
-        await dbClient.destroy();
-    });
-});
-
-describe.each([
-    [0, 1, 0.00],
-    [1, 1, 1.41],
-    [5, 6, 1.29]
-])('startHeight = %s, ga = %s', (startHeight, ga, expectedFallingTime) => {
-    test(`GET FallingTime for startHeight = ${startHeight} and GA = ${ga} in service returns fallingTime = ${expectedFallingTime}`, async () => {
-        const gaData = {
-            name: uuid.v4(),
-            value: ga
-        };
-
-        const mapping = {
-            request: {
-                method: 'GET',
-                urlPattern: `/gravityAcceleration/${gaData.name}`
-            },
-            response: {
-                jsonBody: gaData
-            }
-        }
-
-        await axios.post('http://localhost:5433/__admin/mappings', mapping);
-
-        const response = await axios.get(`http://localhost:5204/FallingTime?startHeight=${startHeight}&gaName=${gaData.name}`);
-
-        expect(response.data['value']).toBeCloseTo(expectedFallingTime, 2);
-    });
-});
+        // Assert
+        expect(response.data['value']).toBeCloseTo(expectedTime, 2)
+    })
+})
 
 describe.each([
     [0, 0.00],
     [1, 0.45],
     [5, 1.01]
-])('startHeight = %s', (startHeight, expectedFallingTime) => {
+])('startHeight = %s', (startHeight, expectedTime) => {
     test('GET FallingTime uses default GA when service is not accessible', async () => {
-        const gaName = uuid.v4();
-        
-        const mapping = {
-            request: {
-                method: 'GET',
-                urlPattern: `/gravityAcceleration/${gaName}`
-            },
-            response: {
-                status: 503
+        // Arrange
+        const gaName = uuid.v4()
+        await configureGravityAccelerationMock503(gaName)
+
+        // Act
+        const url = `http://localhost:5204/FallingTime?startHeight=${startHeight}&gaName=${gaName}`
+        const response = await axios.get(url)
+
+        // Assert
+        expect(response.data['value']).toBeCloseTo(expectedTime, 2)
+    })
+})
+
+describe.each([
+    [0, 1, 0.00],
+    [1, 1, 1.41],
+    [5, 6, 1.29]
+])('startHeight = %s, ga = %s', (startHeight, ga, expectedTime) => {
+    const dbClient = createDbClient()
+
+    test(`GET FallingTime uses GA from DB if possible`, async () => {
+        // Arrange
+        const gaName = uuid.v4()
+        await dbClient
+            .insert({
+                Name: gaName,
+                Value: ga
+            })
+            .into('GravityAcceleration')
+
+        // Act
+        const url = `http://localhost:5204/FallingTime?startHeight=${startHeight}&gaName=${gaName}`
+        const response = await axios.get(url)
+
+        // Assert
+        expect(response.data['value']).toBeCloseTo(expectedTime, 2)
+    })
+})
+
+async function configureGravityAccelerationMockOK(gaName, ga) {
+    const mapping = {
+        request: {
+            method: 'GET',
+            urlPattern: `/gravityAcceleration/${gaName}`
+        },
+        response: {
+            jsonBody: {
+                name: gaName,
+                value: ga
             }
-        };
-        
-        await axios.post('http://localhost:5433/__admin/mappings', mapping);
+        }
+    }
 
-        const response = await axios.get(`http://localhost:5204/FallingTime?startHeight=${startHeight}&gaName=${gaName}`);
+    await axios.post('http://localhost:5433/__admin/mappings', mapping)
+}
 
-        expect(response.data['value']).toBeCloseTo(expectedFallingTime, 2);
-    });
-});
+async function configureGravityAccelerationMock503(gaName) {
+    const mapping = {
+        request: {
+            method: 'GET',
+            urlPattern: `/gravityAcceleration/${gaName}`
+        },
+        response: {
+            status: 503
+        }
+    }
+
+    await axios.post('http://localhost:5433/__admin/mappings', mapping)
+}
+
+function createDbClient() {
+    const dbClient = knex({
+        client: 'pg',
+        connection: {
+            host: 'localhost',
+            port: '5432',
+            user: 'postgres',
+            password: 'js-tests'
+        }
+    })
+
+    afterAll(async () => {
+        await dbClient.destroy()
+    })
+
+    return dbClient
+}
